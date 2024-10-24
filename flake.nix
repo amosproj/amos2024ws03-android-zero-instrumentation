e64c37d52c0b815b245cc21a03ca5aa4641e48b8
Felix Hilgers <felix.hilgers@fau.de>
2024-10-24 14:19:25 +0200

    ci: Include rust dependencies in flake.nix
    Signed-off-by: Felix Hilgers <felix.hilgers@fau.de>

--- flake.nix
+++ flake.nix
@@ -2,14 +2,39 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { self, nixpkgs, fenix }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs { inherit system; };
+      fenixPkgs = fenix.packages.${system};
+
+      rust = {
+        # https://rust-lang.github.io/rustup-components-history/
+        # We need nightly for aya to build ebpf programs
+        nightlyToolchain = fenixPkgs.combine [
+          fenixPkgs.latest.toolchain
+          fenixPkgs.targets.x86_64-linux-android.latest.rust-std
+        ];
+      };
+
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rust.nightlyToolchain
+          protobuf
+          bpf-linker
+        ];
+      };
+    };
 }
