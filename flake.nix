40055b43e49d8cf2f1e3205d4184d93f4b6f4aae
Felix Hilgers <felix.hilgers@fau.de>
2024-10-25 05:30:44 +0200

    ci: Build a layered OCI image
    Using [`nix2container`](https://github.com/nlewo/nix2container) we can
    build layered OCI images with the same inputs that go into our
    development shell.
    
    I have tried to separate the layers into logical parts while trying to
    minimize duplicate paths in the final image. Using a tool like
    [`dive](https://github.com/wagoodman/dive) we can check how efficient
    the image is.
    
    Splitting the layers into logical parts, while being a lot more effort
    that building a single huge blob, makes updating dependencies a lot
    cheaper. Only the affected layers have to be built, pushed and
    downloaded again.
    
    The layers are roughly:
    
    - A shared base layer to minimize duplication
    - The rust toolchain
    - A shared base layer for the Android SDK components
    - Each of the Android SDK components in a separate layer
    - Convenience Tools
    - Ci/Devcontainer specific setup (user, directories, permissions, etc.)
    
    Example with this structure:
    
    Updating the rust toolchain would only affect the rust toolchain layer,
    nothing else.
    
    Updating the Android build tools will also only affect the layer itself
    and not the whole SDK.
    
    In the future we should investigate whether we can split the rust
    toolchain components into their separate layers. At least for each of
    the targets we should do that.
    
    Signed-off-by: Felix Hilgers <felix.hilgers@fau.de>

--- flake.nix
+++ flake.nix
@@ -16,9 +16,13 @@
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
     };
+    nix2container = {
+      url = "github:nlewo/nix2container";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, fenix, android-nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, fenix, android-nixpkgs, nix2container, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         ./nix/overlay-module.nix
@@ -26,8 +30,31 @@
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem = { config, pkgs, system, ... }:
         let
-          androidSdk = {
-            sdk = pkgs.androidSdk (sdkPkgs: with sdkPkgs; [
+          packageGroups = {
+
+            base = with pkgs; [
+              git
+              python3
+              gcc
+              gnugrep
+              gnused
+              gawk
+              findutils
+              iproute2
+              cacert
+              gcc.cc.lib
+              glibc.out
+              coreutils
+              which
+              bash
+            ];
+
+            sdkBase = with pkgs; [
+              ncurses5
+              openjdk
+            ];
+
+            sdkPkgs = with pkgs.androidSdkPackages; [
               cmdline-tools-latest
               ndk-28-0-12433566
               build-tools-35-0-0
@@ -35,27 +62,98 @@
               platforms-android-35
               emulator
               system-images-android-35-default-x86-64
-            ]);
-          };
-          rust = {
-            # https://rust-lang.github.io/rustup-components-history/
-            # We need nightly for aya to build ebpf programs
-            nightlyToolchain = pkgs.fenix.combine (with pkgs.fenix; [
-              latest.toolchain
-              targets.x86_64-linux-android.latest.rust-std
-            ]);
-          };
-        in
-        {
-          devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              rust.nightlyToolchain
+            ];
+
+            rustPkgs = with pkgs.fenix; [
+              (combine [ latest.toolchain targets.x86_64-linux-android.latest.rust-std ])
+            ];
+
+            miscPkgs = with pkgs; [
+              cargo-ndk
               protobuf
               bpf-linker
-              androidSdk.sdk
-              cargo-ndk
-              python3
             ];
+
+            combined =
+              packageGroups.base ++
+              packageGroups.sdkBase ++
+              [ (pkgs.androidSdk (_: packageGroups.sdkPkgs)) ] ++
+              packageGroups.rustPkgs ++
+              packageGroups.miscPkgs;
+          };
+
+          builderBase = pkgs.n2c.buildImage (with pkgs; {
+            name = "ghcr.io/fhilgers/ziofa-builder-base";
+            tag = "latest";
+            copyToRoot = [
+              (buildEnv {
+                name = "root";
+                pathsToLink = [ "/bin" "/lib64" "/lib" "/share" "/etc" ];
+                paths = packageGroups.combined;
+              })
+            ];
+            layers =
+              let
+                mL = deps: layers: n2c.buildLayer { deps = deps; layers = layers; };
+                baseLayer = mL packageGroups.base [ ];
+                miscLayer = mL packageGroups.miscPkgs [ baseLayer ];
+                rustLayer = mL packageGroups.rustPkgs [ baseLayer ];
+                sdkBaseLayer = mL packageGroups.sdkBase [ baseLayer ];
+                sdkLayers = map (sdkPkg: mL [ sdkPkg ] [ sdkBaseLayer baseLayer ]) packageGroups.sdkPkgs;
+              in
+              [
+                baseLayer
+                miscLayer
+                rustLayer
+                sdkBaseLayer
+              ] ++ sdkLayers;
+          });
+
+          dockerHelpers = import ./nix/docker-helpers.nix { pkgs = pkgs; };
+          inherit (dockerHelpers) mkWorker mkDoas mkDoasPerms mkWorkerPerms mkTmp mkHome mkEnv;
+
+          builder = pkgs.n2c.buildImage {
+            name = "ghcr.io/fhilgers/ziofa-builder";
+            tag = "latest";
+            fromImage = builderBase;
+            copyToRoot = [
+              mkTmp
+              mkEnv
+              mkHome
+              mkWorker
+              mkDoas
+
+              pkgs.bashInteractive
+              pkgs.bashConfigs
+            ];
+            perms = [
+              (mkWorkerPerms mkTmp "/tmp")
+              (mkWorkerPerms mkHome "/home/worker")
+              (mkDoasPerms mkDoas "/sbin/doas")
+            ];
+            config = {
+              entrypoint = [ "/bin/bash" ];
+              User = "worker";
+              WorkingDir = "/home/worker";
+              Env = [
+                "PATH=/bin:/sbin:/usr/bin:/usr/sbin"
+                "HOME=/home/worker"
+                "LD_LIBRARY_PATH=/lib:/lib64"
+                "ANDROID_HOME=/share/android-sdk" # deprecated but android-studio needs it
+                "ANDROID_SDK_ROOT=/share/android-sdk"
+                "ANDROID_NDK_HOME=/share/android-sdk/ndk"
+              ];
+            };
+          };
+
+        in
+        {
+          devShells = {
+            default = pkgs.mkShell { packages = packageGroups.combined; };
+          };
+          packages = {
+            dockerBuilderBase = builderBase;
+            dockerBuilder = builder;
           };
         };
     };
