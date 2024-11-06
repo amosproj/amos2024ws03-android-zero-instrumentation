cc2e1671928093ce10092530baaff278513933f8
Felix Hilgers <felix.hilgers@fau.de>
2024-11-06 04:07:23 +0100

    feat: implement kotlin client
    Signed-off-by: Felix Hilgers <felix.hilgers@fau.de>

--- frontend/client/src/main/java/de/amosproj3/ziofa/client/Client.kt
+++ frontend/client/src/main/java/de/amosproj3/ziofa/client/Client.kt
@@ -1,1 +1,45 @@
 package de.amosproj3.ziofa.client
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.shareIn
+
+interface Client {
+    val serverCount: SharedFlow<UInt>
+
+    suspend fun loadProgram(name: String)
+}
+
+interface ClientFactory {
+    suspend fun connect(scope: CoroutineScope, url: String): Client
+}
+
+
+class RustClient(
+    scope: CoroutineScope,
+    private val inner: uniffi.client.Client
+) : Client {
+
+    override val serverCount = flow {
+        val stream = inner.serverCount()
+
+        while (true) {
+            stream.next()?.also {
+                emit(it)
+            } ?: break;
+        }
+    }.shareIn(scope, SharingStarted.Lazily)
+
+    override suspend fun loadProgram(name: String) {
+        inner.loadProgram(name)
+    }
+}
+
+class RustClientFactory : ClientFactory {
+    override suspend fun connect(scope: CoroutineScope, url: String): Client {
+        return RustClient(scope, uniffi.client.Client.connect(url))
+    }
+}
