67b809c8d121eeded564a9d003e9edd924d31032
Felix Hilgers <felix.hilgers@fau.de>
2024-11-06 04:09:41 +0100

    feat: wire up app with client lib
    Signed-off-by: Felix Hilgers <felix.hilgers@fau.de>

--- frontend/app/src/main/java/de/amosproj3/ziofa/ZIOFAApplication.kt
+++ frontend/app/src/main/java/de/amosproj3/ziofa/ZIOFAApplication.kt
@@ -5,15 +5,19 @@
 package de.amosproj3.ziofa
 
 import android.app.Application
+import de.amosproj3.ziofa.client.ClientFactory
+import de.amosproj3.ziofa.client.RustClientFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 class ZIOFAApplication : Application() {
 
     val appModule = module {
         // TODO add declarations here
+        singleOf<ClientFactory>(::RustClientFactory)
     }
 
     override fun onCreate() {
