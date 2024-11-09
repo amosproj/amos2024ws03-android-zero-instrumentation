8b45152b2525fada523c99c3cd85c734c2a51e25
Tom Weisshuhn <tom.weisshuhn@fau.de>
2024-11-09 19:11:33 +0100

    [none working] implemented kprobe for vfs_write
    Signed-off-by: Tom Weisshuhn <tom.weisshuhn@fau.de>

--- rust/example-ebpf/src/main.rs
+++ rust/example-ebpf/src/main.rs
@@ -7,13 +7,34 @@
 //
 // SPDX-License-Identifier: MIT
 
-use aya_ebpf::{bindings::xdp_action, macros::{map, xdp}, maps::{PerCpuArray, RingBuf}, programs::XdpContext};
+use aya_ebpf::{bindings::xdp_action, macros::{map, xdp, kprobe}, maps::{PerCpuArray, RingBuf}, programs::{XdpContext, ProbeContext}, EbpfContext};
+use example_common::{KProbeData, KProbeTypes};
 
 #[map(name="COUNTER")]
 static PACKET_COUNTER: PerCpuArray<u32> = PerCpuArray::with_max_entries(1, 0);
 
 #[map(name="EVENTS")]
 static EVENTS: RingBuf = RingBuf::with_byte_size(1024, 0);
+
+#[map(name="KProbes")]
+static KPROBES: RingBuf = RingBuf::with_byte_size(1024, 0);
+
+#[kprobe]
+pub fn vfs_write(ctx: ProbeContext) -> Result<(), u32> {
+    let pid = ctx.pid();
+    let tid = ctx.tgid();
+
+    let data = KProbeData{pid, tid, probe_type: KProbeTypes::VfsWrite};
+    let mut entry = match KPROBES.reserve::<KProbeData>(0) {
+        Some(entry) => entry,
+        None => return Err(0)
+    };
+
+    entry.write(data);
+    entry.submit(0);
+
+    Ok(())
+}
 
 #[xdp]
 pub fn example(ctx: XdpContext) -> u32 {
