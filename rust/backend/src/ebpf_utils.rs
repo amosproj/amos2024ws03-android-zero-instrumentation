cf0ca5b53e747aeb3f75739ccc89c8895bd065bb
ffranzgitHub <franz.schlicht@gmail.com>
2024-11-12 20:24:46 +0100

    Implement update_from_config()
    Each time the configuration gets set, all its entries will get attached or dettached according to their "attach" boolean.
    
    Co-authored-by: Mr-Kanister <68117355+mr-kanister@users.noreply.github.com>
    Signed-off-by: ffranzgitHub <franz.schlicht@gmail.com>

--- rust/backend/src/ebpf_utils.rs
+++ rust/backend/src/ebpf_utils.rs
@@ -5,29 +5,20 @@
 // SPDX-License-Identifier: MIT
 
 use std::collections::HashMap;
+use std::sync::MutexGuard;
 use aya::programs::kprobe::KProbeLinkId;
 use aya::programs::uprobe::UProbeLinkId;
 use aya::programs::UProbe;
 use aya::{include_bytes_aligned, programs::KProbe, Ebpf};
 use libc::{pid_t};
+use shared::config::ebpf_entry::UprobeConfig;
 use crate::configuration::load_from_file;
-
-enum ProbeType{
-    UPROBE {
-        offset: u64,
-        target: String,
-        pid: Option<pid_t>,
-    },
-    KPROBE,
-    KRETPROBE,
-}
-
 pub enum ProbeID {
     KProbeID(KProbeLinkId),
     UProbeID(UProbeLinkId),
 }
 fn load_function(ebpf: &mut Ebpf, hash_map: &mut HashMap<String, ProbeID>,
-                 probe_type: ProbeType, func: &str, hook: &str) {
+                 probe_type: Option<UprobeConfig>, func: &str, hook: &str) {
     /* examples:
      * func: "kprobetcp"
      * hook: "tcp_connect"
@@ -36,11 +27,12 @@ fn load_function(ebpf: &mut Ebpf, hash_map: &mut HashMap<String, ProbeID>,
     // TODO: Error checking
 
     match probe_type {
-        ProbeType::UPROBE {
+        // UPROBE
+        Some(UprobeConfig {
             offset,
             target,
             pid,
-        } => {
+        }) => {
             // get ebpf program
             let program: &mut UProbe = ebpf.program_mut(func).unwrap().try_into().unwrap();
 
@@ -51,7 +43,8 @@ fn load_function(ebpf: &mut Ebpf, hash_map: &mut HashMap<String, ProbeID>,
             hash_map.insert(func.to_string(),
                             ProbeID::UProbeID(program.attach(Some(hook), offset, target, pid).unwrap()));
         }
-        ProbeType::KPROBE | ProbeType::KRETPROBE => {
+        // KPROBE/KRETPROBE
+        None => {
             let program: &mut KProbe = ebpf.program_mut(func).unwrap().try_into().unwrap();
             program.load().unwrap();
             hash_map.insert(func.to_string(),
@@ -79,7 +72,24 @@ fn unload_function(ebpf: &mut Ebpf, hash_map: &mut HashMap<String, ProbeID>, fun
     }
 }
 
-// TODO
-pub fn update_from_config(config_path: &str, loaded_functions: HashMap<String, ProbeID>) {
-    let config = load_from_file(config_path).unwrap().entries;
+pub fn update_from_config(ebpf: &mut Ebpf, config_path: &str, loaded_functions: &mut HashMap<String, ProbeID>) {
+    let entries = load_from_file(config_path).unwrap().entries;
+
+    for entry in entries {
+        if entry.attach {
+            match loaded_functions.get(entry.ebpf_name.as_str()) {
+                Some(res) => {}
+                None => {
+                    load_function(ebpf, loaded_functions, entry.uprobe_info, entry.ebpf_name.as_str(), entry.hook.as_str())
+                }
+            }
+        } else {
+            match loaded_functions.get(entry.ebpf_name.as_str()) {
+                Some(res) => {
+                    unload_function(ebpf, loaded_functions, entry.ebpf_name.as_str());
+                },
+                None => {}
+            }
+        }
+    }
 }
--- rust/backend/src/server.rs
+++ rust/backend/src/server.rs
@@ -2,7 +2,8 @@
 //
 // SPDX-License-Identifier: MIT
 
-use crate::{constants, configuration, ebpf_utils::ProbeID};
+use crate::{constants, configuration};
+use crate::ebpf_utils::{ProbeID, update_from_config};
 use shared::config::Configuration;
 use shared::ziofa::ziofa_server::{Ziofa, ZiofaServer};
 use shared::ziofa::{
@@ -12,11 +13,21 @@ use shared::ziofa::{
 };
 use tonic::{transport::Server, Request, Response, Status};
 use std::collections::HashMap;
+use std::ops::DerefMut;
+use std::sync::Arc;
+use aya::Ebpf;
+use tokio::sync::Mutex;
 
-#[derive(Default)]
 pub struct ZiofaImpl {
     // tx: Option<Sender<Result<EbpfStreamObject, Status>>>,
-    probe_id_map: HashMap<String, ProbeID>,
+    probe_id_map: Arc<Mutex<HashMap<String, ProbeID>>>,
+    ebpf: Arc<Mutex<Ebpf>>,
+}
+
+impl ZiofaImpl {
+    pub fn new(probe_id_map: HashMap<String, ProbeID>, ebpf: Ebpf) -> ZiofaImpl {
+        ZiofaImpl { probe_id_map: Arc::new(Mutex::new(probe_id_map)), ebpf: Arc::new(Mutex::new(ebpf)) }
+    }
 }
 
 #[tonic::async_trait]
@@ -64,6 +75,11 @@ impl Ziofa for ZiofaImpl {
         // TODO: if ? fails needs valid return value for the function so that the server doesn't fail
         configuration::validate(&config)?;
         configuration::save_to_file(&config, constants::DEV_DEFAULT_FILE_PATH)?;
+
+        let mut ebpf_guard = self.ebpf.lock().await;
+        let mut probe_id_map_guard = self.probe_id_map.lock().await;
+        update_from_config(ebpf_guard.deref_mut(), &config, probe_id_map_guard.deref_mut());
+
         Ok(Response::new(SetConfigurationResponse{ response_type: 0}))
     }
 
@@ -79,7 +95,12 @@ impl Ziofa for ZiofaImpl {
 }
 
 pub async fn serve_forever() {
-    let service = ZiofaServer::new(ZiofaImpl::default());
+    let mut ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
+        env!("OUT_DIR"),
+        "/example"
+    ))).unwrap();
+    let mut probe_id_map = HashMap::new();
+    let service = ZiofaServer::new(ZiofaImpl::new(probe_id_map, ebpf));
     Server::builder()
         .add_service(service)
         .serve(constants::sock_addr())
