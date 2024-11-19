8d36eee63d6bbc1a51db80db304aa4e9a60bb069
Felix Hilgers <felix.hilgers@fau.de>
2024-11-19 18:40:08 +0100

    test(daemon): listing processes
    Signed-off-by: Felix Hilgers <felix.hilgers@fau.de>

---
+++ rust/backend/daemon/src/lib.rs
@@ -0,0 +1,13 @@
+mod configuration;
+mod constants;
+pub mod counter;
+mod ebpf_utils;
+mod helpers;
+mod procfs_utils;
+mod server;
+
+pub async fn run_server() {
+    helpers::bump_rlimit();
+
+    server::serve_forever().await;
+} 
