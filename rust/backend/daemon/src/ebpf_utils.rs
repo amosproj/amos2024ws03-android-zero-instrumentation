7ec4463cb4852bec4e44c41ad71c8e672e96de7f
Mr-Kanister <68117355+Mr-Kanister@users.noreply.github.com>
2024-11-24 16:46:17 +0100

    [None working] Changed proto files and started to redesign ebpf_utils
    Signed-off-by: Mr-Kanister <68117355+Mr-Kanister@users.noreply.github.com>

--- rust/backend/daemon/src/ebpf_utils.rs
+++ rust/backend/daemon/src/ebpf_utils.rs
@@ -4,101 +4,151 @@
 //
 // SPDX-License-Identifier: MIT
 
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use aya::{
+    maps::RingBuf,
     programs::{kprobe::KProbeLinkId, uprobe::UProbeLinkId, KProbe, UProbe},
     Ebpf,
 };
-use shared::config::ebpf_entry::UprobeConfig;
+use clap::Error;
+use shared::config::UprobeConfig;
+use tokio::{io::unix::AsyncFd, sync::Mutex};
 
 use crate::configuration::load_from_file;
 pub enum ProbeID {
     KProbeID(KProbeLinkId),
     UProbeID(UProbeLinkId),
 }
-fn load_function(
-    ebpf: &mut Ebpf,
-    hash_map: &mut HashMap<String, ProbeID>,
-    probe_type: Option<UprobeConfig>,
-    func: &str,
-    hook: &str,
-) {
-    /* examples:
-     * func: "kprobetcp"
-     * hook: "tcp_connect"
-     */
 
-    // TODO: Error checking
+struct VfsFeature {
+    vfs_write_id: KProbeLinkId,
+    vfs_write_ret_id: KProbeLinkId,
+}
 
-    match probe_type {
-        // UPROBE
-        Some(UprobeConfig {
-            offset,
-            target,
-            pid,
-        }) => {
-            // get ebpf program
-            let program: &mut UProbe = ebpf.program_mut(func).unwrap().try_into().unwrap();
+impl VfsFeature {
+    fn create(&mut self, ebpf: &mut Ebpf) {
+        // TODO Error handling
+        let vfs_write: &mut KProbe = ebpf.program_mut("vfs_write").unwrap().try_into().unwrap();
+        vfs_write.load();
+        self.vfs_write_id = vfs_write.attach("vfs_write", 0).unwrap();
 
-            // load ebpf program
-            program.load().unwrap();
+        let vfs_write_ret: &mut KProbe = ebpf
+            .program_mut("vfs_write_ret")
+            .unwrap()
+            .try_into()
+            .unwrap();
+        vfs_write_ret.load();
+        self.vfs_write_ret_id = vfs_write_ret.attach("vfs_write", 0).unwrap();
+    }
 
-            // attach ebpf program and insert its ProbeID into the hash map
-            hash_map.insert(
-                func.to_string(),
-                ProbeID::UProbeID(program.attach(Some(hook), offset, target, pid).unwrap()),
-            );
-        }
-        // KPROBE/KRETPROBE
-        None => {
-            let program: &mut KProbe = ebpf.program_mut(func).unwrap().try_into().unwrap();
-            program.load().unwrap();
-            hash_map.insert(
-                func.to_string(),
-                ProbeID::KProbeID(program.attach(hook, 0).unwrap()),
-            );
-        }
+    fn update(&mut self, ebpf: &mut Ebpf) {
+        // update pids that are attached
+        !todo!();
+    }
+
+    fn events(&mut self, ebpf: &mut Ebpf) {
+        // return buffered stream of events
+        // will be discussed by Felix and Beni
+        !todo!()
+    }
+
+    fn destroy(ebpf: &mut Ebpf) {
+        // TODO Error handling
+        let vfs_write: &mut KProbe = ebpf.program_mut("vfs_write").unwrap().try_into().unwrap();
+        vfs_write.unload();
+
+        let vfs_write_ret: &mut KProbe = ebpf
+            .program_mut("vfs_write_ret")
+            .unwrap()
+            .try_into()
+            .unwrap();
+        vfs_write_ret.unload();
     }
 }
 
-fn unload_function(ebpf: &mut Ebpf, hash_map: &mut HashMap<String, ProbeID>, func: &str) {
-    // get ProbeID and remove it from hash map
-    let probe = hash_map.remove(func).unwrap();
 
-    match probe {
-        ProbeID::UProbeID(_link_id) => {
-            // get ebpf program
-            let program: &mut UProbe = ebpf.program_mut(func).unwrap().try_into().unwrap();
+// fn load_function(
+//     ebpf: &mut Ebpf,
+//     hash_map: &mut HashMap<String, ProbeID>,
+//     probe_type: Option<UprobeConfig>,
+//     func: &str,
+//     hook: &str,
+// ) {
+//     /* examples:
+//      * func: "kprobetcp"
+//      * hook: "tcp_connect"
+//      */
+//     // TODO: Error checking
 
-            // unload ebpf program
-            program.unload().unwrap();
-        }
-        ProbeID::KProbeID(_link_id) => {
-            let program: &mut KProbe = ebpf.program_mut(func).unwrap().try_into().unwrap();
-            program.unload().unwrap();
-        }
-    }
-}
+//     match probe_type {
+//         // UPROBE
+//         Some(UprobeConfig {
+//             offset,
+//             target,
+//             pid,
+//         }) => {
+//             // get ebpf program
+//             let program: &mut UProbe = ebpf.program_mut(func).unwrap().try_into().unwrap();
 
-pub fn update_from_config(
-    ebpf: &mut Ebpf,
-    config_path: &str,
-    loaded_functions: &mut HashMap<String, ProbeID>,
-) {
-    let entries = load_from_file(config_path).unwrap().entries;
+//             // load ebpf program
+//             program.load().unwrap();
 
-    for entry in entries {
-        match (entry.attach, loaded_functions.get(entry.ebpf_name.as_str())) {
-            (true, None) => load_function(
-                ebpf,
-                loaded_functions,
-                entry.uprobe_info,
-                entry.ebpf_name.as_str(),
-                entry.hook.as_str(),
-            ),
-            (false, Some(_)) => unload_function(ebpf, loaded_functions, entry.ebpf_name.as_str()),
-            _ => {}
-        }
-    }
-}
+//             // attach ebpf program and insert its ProbeID into the hash map
+//             hash_map.insert(
+//                 func.to_string(),
+//                 ProbeID::UProbeID(program.attach(Some(hook), offset, target, pid).unwrap()),
+//             );
+//         }
+//         // KPROBE/KRETPROBE
+//         None => {
+//             let program: &mut KProbe = ebpf.program_mut(func).unwrap().try_into().unwrap();
+//             program.load().unwrap();
+//             hash_map.insert(
+//                 func.to_string(),
+//                 ProbeID::KProbeID(program.attach(hook, 0).unwrap()),
+//             );
+//         }
+//     }
+// }
+
+// fn unload_function(ebpf: &mut Ebpf, hash_map: &mut HashMap<String, ProbeID>, func: &str) {
+//     // get ProbeID and remove it from hash map
+//     let probe = hash_map.remove(func).unwrap();
+
+//     match probe {
+//         ProbeID::UProbeID(_link_id) => {
+//             // get ebpf program
+//             let program: &mut UProbe = ebpf.program_mut(func).unwrap().try_into().unwrap();
+
+//             // unload ebpf program
+//             program.unload().unwrap();
+//         }
+//         ProbeID::KProbeID(_link_id) => {
+//             let program: &mut KProbe = ebpf.program_mut(func).unwrap().try_into().unwrap();
+//             program.unload().unwrap();
+//         }
+//     }
+// }
+
+// pub fn update_from_config(
+//     ebpf: &mut Ebpf,
+//     config_path: &str,
+//     loaded_functions: &mut HashMap<String, ProbeID>,
+// ) {
+//     let entries = load_from_file(config_path).unwrap().entries;
+
+//     for entry in entries {
+//         match (entry.attach, loaded_functions.get(entry.ebpf_name.as_str())) {
+//             (true, None) => load_function(
+//                 ebpf,
+//                 loaded_functions,
+//                 entry.uprobe_info,
+//                 entry.ebpf_name.as_str(),
+//                 entry.hook.as_str(),
+//             ),
+//             (false, Some(_)) => unload_function(ebpf, loaded_functions, entry.ebpf_name.as_str()),
+//             _ => {}
+//         }
+//     }
+// }
