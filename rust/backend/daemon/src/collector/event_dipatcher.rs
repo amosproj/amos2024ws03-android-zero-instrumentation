3ed8dbe5888c34425079704d823c2305644f3e03
Felix Hilgers <felix.hilgers@fau.de>
2024-12-11 08:34:20 +0100

    refactor: replace collector with proper actors
    The actor model is a design pattern which fits our usecase very well.
    Especially with the new registry, the pattern feels much better than
    manually baking it ourselves.
    
    Signed-off-by: Felix Hilgers <felix.hilgers@fau.de>

---
+++ rust/backend/daemon/src/collector/event_dipatcher.rs
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: 2024 Felix Hilgers <felix.hilgers@fau.de>
+//
+// SPDX-License-Identifier: MIT
+
+use async_broadcast::Sender;
+use ractor::Actor;
+use shared::ziofa::Event;
+use tonic::Status;
+
+
+
+pub struct EventDispatcher;
+
+pub struct EventDispatcherState {
+    destination: Sender<Result<Event, Status>>,
+}
+
+impl EventDispatcherState {
+    pub fn new(destination: Sender<Result<Event, Status>>) -> Self {
+        Self { destination }
+    }
+}
+
+
+impl Actor for EventDispatcher {
+    type Arguments = EventDispatcherState;
+    type Msg = Event;
+    type State = EventDispatcherState;
+    
+    async fn pre_start(
+            &self,
+            _: ractor::ActorRef<Self::Msg>,
+            args: Self::Arguments,
+        ) -> Result<Self::State, ractor::ActorProcessingErr> {
+        Ok(args)
+    }
+    
+    async fn handle(
+            &self,
+            _: ractor::ActorRef<Self::Msg>,
+            message: Self::Msg,
+            state: &mut Self::State,
+        ) -> Result<(), ractor::ActorProcessingErr> {
+        state.destination.broadcast_direct(Ok(message))
+            .await?;
+        
+        Ok(())
+    }
+}
