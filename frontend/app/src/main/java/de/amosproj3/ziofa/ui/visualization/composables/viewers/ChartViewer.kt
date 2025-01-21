9a7d16c88a8fb763fbffdebd55a2324f72a4211e
Luca Bretting <luca.bretting@fau.de>
2025-01-21 22:33:15 +0100

    fix: align visualization in chart mode
    Signed-off-by: Luca Bretting <luca.bretting@fau.de>

--- frontend/app/src/main/java/de/amosproj3/ziofa/ui/visualization/composables/viewers/ChartViewer.kt
+++ frontend/app/src/main/java/de/amosproj3/ziofa/ui/visualization/composables/viewers/ChartViewer.kt
@@ -4,7 +4,9 @@
 
 package de.amosproj3.ziofa.ui.visualization.composables.viewers
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import de.amosproj3.ziofa.ui.visualization.composables.chart.VicoBar
 import de.amosproj3.ziofa.ui.visualization.composables.chart.VicoTimeSeries
 import de.amosproj3.ziofa.ui.visualization.data.GraphedData
@@ -13,7 +15,11 @@ import de.amosproj3.ziofa.ui.visualization.data.GraphedData
 fun ChartViewer(data: GraphedData) {
     when (data) {
         is GraphedData.TimeSeriesData ->
-            VicoTimeSeries(seriesData = data.seriesData, chartMetadata = data.metaData)
+            VicoTimeSeries(
+                seriesData = data.seriesData,
+                chartMetadata = data.metaData,
+                modifier = Modifier.fillMaxSize(),
+            )
 
         is GraphedData.HistogramData ->
             VicoBar(seriesData = data.seriesData, chartMetadata = data.metaData)
